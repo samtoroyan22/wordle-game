67a4ef71075d2faaf9aa0645bb11f501d4948153
Sam <samtoroyan777@gmail.com>
2025-06-01 14:19:25 +0300

    Configure Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/wordle-game/", // Укажи имя твоего репозитория
   plugins: [react()],
-})
+});
